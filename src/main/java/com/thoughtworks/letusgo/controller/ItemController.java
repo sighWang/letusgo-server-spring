857b783e4f4f12d8fbec7868fbb1ec8057b868c6
WangXiming <wangximingde@163.com>
2014-11-14 15:04:10 +0800

    chaneg @Controller to @RestController

--- src/main/java/com/thoughtworks/letusgo/controller/ItemController.java
+++ src/main/java/com/thoughtworks/letusgo/controller/ItemController.java
@@ -4,12 +4,11 @@ import com.thoughtworks.letusgo.domain.Item;
 import com.thoughtworks.letusgo.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/items")
 public class ItemController {
 
@@ -17,13 +16,11 @@ public class ItemController {
     private ItemService itemService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    @ResponseBody
     public List<Item> getItems() {
         return itemService.getItems();
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    @ResponseBody
     public Item getItemById(@PathVariable("id") int id) {
         return itemService.getItemById(id);
     }
