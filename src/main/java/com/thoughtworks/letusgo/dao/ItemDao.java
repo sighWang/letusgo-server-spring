cca99f27061e5b2778532f8e3c1283c5a981762e
WangXiming <wangximingde@163.com>
2014-11-15 09:20:52 +0800

    update editItem

--- src/main/java/com/thoughtworks/letusgo/dao/ItemDao.java
+++ src/main/java/com/thoughtworks/letusgo/dao/ItemDao.java
@@ -72,19 +72,10 @@ public class ItemDao {
         jdbcTemplate.update(sql, item.getBarcode(), item.getName(),
                 item.getUnit(), item.getPrice(), item.getCategory().getId());
     }
-    //TODO: need to be edit
     public void editItem(Item item) {
         String sql = "UPDATE item SET barcode = ?, name = ?, unit = ?, price = ?, category_id = ? " +
                 " WHERE id = ?";
-        System.out.println("barcdoe" + item.getBarcode() + "name"+item.getName()+
-              " unit"+   item.getUnit() + "price" +item.getPrice()+"id" +item.getCategory().getId() +"id" + item.getId());
-        String barcode = item.getBarcode();
-        String name = item.getName();
-        String unit = item.getUnit();
-        double price = item.getPrice();
-        int categoryId = item.getCategory().getId();
-        int id = item.getId();
-        jdbcTemplate.update(sql, barcode, name, unit, price,categoryId, id);
+        jdbcTemplate.update(sql, item.getBarcode(), item.getName(), item.getUnit(), item.getPrice(),item.getCategory().getId(), item.getId());
     }
 
     public void removeItem(int id) {
